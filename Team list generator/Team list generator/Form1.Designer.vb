723cb342209f034ecd6aeed074cfba17bbc28575
Stéphan Taljaard <steptaljaard@gmail.com>
2015-05-15 15:58:40 +0200

    Add first batch of form controls

--- Team list generator/Team list generator/Form1.Designer.vb
+++ Team list generator/Team list generator/Form1.Designer.vb
@@ -23,21 +23,113 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.tlp_form = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.tlp_form.SuspendLayout()
         Me.SuspendLayout()
         '
         'tlp_form
         '
         Me.tlp_form.ColumnCount = 2
-        Me.tlp_form.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.tlp_form.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlp_form.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tlp_form.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tlp_form.Controls.Add(Me.Label1, 0, 0)
+        Me.tlp_form.Controls.Add(Me.TextBox1, 1, 0)
+        Me.tlp_form.Controls.Add(Me.Label2, 0, 1)
+        Me.tlp_form.Controls.Add(Me.TextBox2, 1, 1)
+        Me.tlp_form.Controls.Add(Me.Label3, 0, 2)
+        Me.tlp_form.Controls.Add(Me.DateTimePicker1, 1, 2)
+        Me.tlp_form.Controls.Add(Me.Label4, 0, 3)
+        Me.tlp_form.Controls.Add(Me.TextBox3, 1, 3)
         Me.tlp_form.Dock = System.Windows.Forms.DockStyle.Fill
         Me.tlp_form.Location = New System.Drawing.Point(0, 0)
         Me.tlp_form.Name = "tlp_form"
-        Me.tlp_form.RowCount = 2
-        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlp_form.RowCount = 5
+        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.tlp_form.Size = New System.Drawing.Size(284, 261)
         Me.tlp_form.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(74, 3)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Player ID"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox2.Location = New System.Drawing.Point(74, 29)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Date of birth"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DateTimePicker1.Location = New System.Drawing.Point(74, 55)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(207, 20)
+        Me.DateTimePicker1.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(3, 84)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Event name"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox3.Location = New System.Drawing.Point(74, 81)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox3.TabIndex = 7
         '
         'Form1
         '
@@ -47,9 +139,19 @@ Partial Class Form1
         Me.Controls.Add(Me.tlp_form)
         Me.Name = "Form1"
         Me.Text = "Pokémon VG team list generator"
+        Me.tlp_form.ResumeLayout(False)
+        Me.tlp_form.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents tlp_form As TableLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
 End Class
