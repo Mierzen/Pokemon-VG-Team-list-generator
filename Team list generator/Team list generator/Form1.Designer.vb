08f4ddc2806f662f4b9273cb97a404bd8cd3890d
Stéphan Taljaard <steptaljaard@gmail.com>
2015-05-15 15:48:23 +0200

    Add TableLayoutPanel, rename form

--- Team list generator/Team list generator/Form1.Designer.vb
+++ Team list generator/Team list generator/Form1.Designer.vb
@@ -22,9 +22,34 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.tlp_form = New System.Windows.Forms.TableLayoutPanel()
+        Me.SuspendLayout()
+        '
+        'tlp_form
+        '
+        Me.tlp_form.ColumnCount = 2
+        Me.tlp_form.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlp_form.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlp_form.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tlp_form.Location = New System.Drawing.Point(0, 0)
+        Me.tlp_form.Name = "tlp_form"
+        Me.tlp_form.RowCount = 2
+        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlp_form.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlp_form.Size = New System.Drawing.Size(284, 261)
+        Me.tlp_form.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.tlp_form)
+        Me.Name = "Form1"
+        Me.Text = "Pokémon VG team list generator"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents tlp_form As TableLayoutPanel
 End Class
