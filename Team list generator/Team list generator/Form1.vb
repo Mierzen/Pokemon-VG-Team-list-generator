a6730b56d91de55904d50142b83375ea90674f1c
Stéphan Taljaard <steptaljaard@gmail.com>
2015-05-15 16:55:40 +0200

    Begin input validation

--- Team list generator/Team list generator/Form1.vb
+++ Team list generator/Team list generator/Form1.vb
@@ -1,3 +1,32 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports iTextSharp.text.pdf
+Imports System.Text.RegularExpressions
 
+Public Class Form1
+    Dim age As Integer
+
+#Region "Validation"
+    Private Sub tb_name_Leave(sender As Object, e As EventArgs) Handles tb_name.Leave
+        If Not Regex.Match(tb_name.Text, "^[a-z]*$", RegexOptions.IgnoreCase).Success Or tb_name.Text = "" Then
+            MsgBox("Please enter only alphabetic characters.", vbOKOnly Or vbCritical, "Incorrect name")
+            tb_name.Focus()
+            tb_name.SelectAll()
+        End If
+    End Sub
+
+    Private Sub tb_playerID_Leave(sender As Object, e As EventArgs) Handles tb_playerID.Leave
+        If Strings.Len(tb_playerID.Text) <> 7 Then
+            MsgBox("Incorrect Player ID entered." & vbNewLine & "Please correct your Player ID.", vbOKOnly Or vbCritical, "Incorrect Player ID")
+            tb_playerID.Focus()
+            tb_playerID.SelectAll()
+        End If
+    End Sub
+
+    Private Sub dtp_dateOfBirth_Leave(sender As Object, e As EventArgs) Handles dtp_dateOfBirth.Leave
+        age = Date.Today.Year - dtp_dateOfBirth.Value.Year
+        If age < 5 Then
+            MsgBox("Please check your date of birth.", vbOKOnly Or vbCritical, "Incorrect player age")
+        End If
+    End Sub
+#End Region
 End Class
